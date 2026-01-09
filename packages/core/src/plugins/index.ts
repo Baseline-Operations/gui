137bfcc1efc5772bb7a84698cd1df4ddfaaaa9d8
chrisbrine <chrisbrine.web@gmail.com>
2026-01-08 19:54:35 -0800

    Add .gitignore and remove build artifacts and monorepo files

--- packages/core/src/plugins/index.ts
+++
@@ -1,8 +0,0 @@
-export * from './types.js';
-export * from './manager.js';
-export * from './builtin/index.js';
-export * from './languages/index.js';
-export * from './providers/index.js';
-export * from './package-managers/index.js';
-export * from './editors/index.js';
-
